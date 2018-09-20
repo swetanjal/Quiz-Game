9b1e5e839340c364414b54ee1a486d6f4e9b1ad9
Swetanjal Dutta <swetanjal.dutta@research.iiit.ac.in>
2018-09-21 02:51:52 +0530

    Add authenticate function to backend.

--- go/src/CRUD_API.go
+++ go/src/CRUD_API.go
@@ -69,8 +69,10 @@ func main() {
 	r.POST("/record", PostRecord)
 	r.GET("/record", GetHistory)
 	r.GET("/questions/:id", GetQuestions)
+	r.POST("/authenticate", Authenticate)
 	/*r.PUT("/people/:id", UpdatePerson)
 	  r.DELETE("/people/:id", DeletePerson)*/
+
 	r.Use((cors.Default()))
 	r.Run(":8080") // Run on port 8080
 }
@@ -98,6 +100,21 @@ func UpdatePerson(c *gin.Context) {
    c.JSON(200, person)
 }
 */
+func Authenticate(c *gin.Context) {
+	var user User
+	c.BindJSON(&user)
+	username := user.Username
+	password := user.Password
+	var success User
+	if err := db.Where("Username = ?", username).Where("Password = ?", password).Find(&success).Error; err != nil {
+		c.Header("access-control-allow-origin", "*")
+		c.JSON(200, success)
+	} else {
+		c.Header("access-control-allow-origin", "*") // Why am I doing this? Find out. Try running with this line commented
+		c.JSON(200, success)
+	}
+}
+
 func Valid_Username(c *gin.Context) {
 	username := c.Params.ByName("username")
 	var user User
